851b45a73cdc5a7b48d805dde00e880d4d9867eb
Bassam Ibrahim <100430441+b4554m@users.noreply.github.com>
2022-10-26 09:47:40 +0200

    Update App.js

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import MainPage from './Components/MainPage';
 import CalcCompanion from './Components/CalcCompanion';
 import TestComponent from './Components/TestComponent'
 import { useState } from 'react';
+import { Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+  
   const [counter, setCounter] = useState(0);
   return (
+
     <div className="App">
     <MainPage />
     </div>
